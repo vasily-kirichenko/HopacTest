629d6166a2ba284e31011a6d2b4e781d0bb500ab
Vasily Kirichenko <Vasily.Kirichenko@kaspersky.com>
2014-12-02 17:16:29 +0300

    more interesting WorkersPool test

--- HopacTest/WorkersPool2.fsx
+++ HopacTest/WorkersPool2.fsx
@@ -6,7 +6,7 @@ open Hopac
 open Hopac.Job.Infixes
 open Hopac.Alt.Infixes
 
-type Pool<'msg, 'res>(degreeOfParallelism: int, source: Alt<'msg>, worker: 'msg -> Choice<'res, 'msg * exn>) =
+type Pool<'msg, 'res>(degreeOfParallelism: int, source: Alt<'msg>, worker: 'msg -> Job<Choice<'res, 'msg * exn>>) =
     let degreeOfParallelism = MVar.Now.createFull degreeOfParallelism
     let dopChanged = ch<unit>()
     let workDone = ch<Choice<'res, 'msg * exn>>()
@@ -21,7 +21,7 @@ type Pool<'msg, 'res>(degreeOfParallelism: int, source: Alt<'msg>, worker: 'msg 
     
     let pool = Job.iterateServer 0 <| fun workerCount ->
         Alt.select [ getMessage workerCount >>=? fun msg -> 
-                         job { let res = worker msg
+                         job { let! res = worker msg
                                return! Ch.give workDone res } |> Job.start >>% workerCount + 1
                      workDone >>=? fun res ->
                         match res with
@@ -34,8 +34,15 @@ type Pool<'msg, 'res>(degreeOfParallelism: int, source: Alt<'msg>, worker: 'msg 
         MVar.take degreeOfParallelism >>. MVar.fill degreeOfParallelism value >>. Ch.give dopChanged () |> start
 
 module Test =
+    open System
+
     let mb = mb<int>()
-    let pool = Pool<int, unit>(3, mb, (fun msg -> printfn "[worker] Received %A" msg; Choice1Of2()))
+    
+    let pool = Pool<int, unit>(3, mb, (fun msg -> job {
+                   printfn "[worker] Received %A. Sleeping..." msg
+                   do! Timer.Global.sleep (TimeSpan.FromMinutes 10.)
+                   return Choice1Of2() }))
+
     start (Mailbox.send mb 1)
 
 
