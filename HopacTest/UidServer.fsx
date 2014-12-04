632c0efccf191b78fba4e2363a79aa27ef247d5c
vasily-kirichenko <vaskir@gmail.com>
2014-12-04 23:01:31 +0300

    *

--- HopacTest/UidServer.fsx
+++ HopacTest/UidServer.fsx
@@ -3,21 +3,43 @@
 
 open System
 open Hopac
+open Hopac.Infixes
 open Hopac.Job.Infixes
 open Hopac.Alt.Infixes
 
-let uidServer = Job.delay <| fun _ ->
+let simpleUidServer = Job.delay <| fun _ ->
     let channel = ch()
     Job.iterateServer 0 (fun value ->
         Ch.give channel value >>% value + 1) >>% 
     channel
 
-let server = run uidServer
+let uidServerWithNack: Ch<int * Alt<unit> * Ch<int>> = 
+    let reqCh = ch()
+    server << Job.iterate 0 <| fun oldValue ->
+        reqCh >>= fun (n, nack, replyCh) ->
+            let newValue = oldValue + n
+            (replyCh <-? newValue >>%? newValue) <|>
+            (nack >>%? oldValue)
+    reqCh 
+
+let incrementBy n = Alt.withNack <| fun nack ->
+    let replyCh = ch()
+    uidServerWithNack <-+ (n, nack, replyCh) >>% upcast replyCh
 
 seq { 1..1000000 }
-|> Seq.map (fun _ -> Ch.take server)
+|> Seq.map (fun _ -> (incrementBy 1) <|> (incrementBy 2))
 |> Job.conCollect
 |> run
 |> Seq.sumBy (fun x -> int64 x)
  
-run (Job.conCollect [ Ch.take server; Ch.take server ])
+//run (Job.conCollect [ Ch.take server; Ch.take server ])
+
+//let server = run simpleUidServer
+//
+//seq { 1..1000000 }
+//|> Seq.map (fun _ -> Ch.take server)
+//|> Job.conCollect
+//|> run
+//|> Seq.sumBy (fun x -> int64 x)
+// 
+//run (Job.conCollect [ Ch.take server; Ch.take server ])
--- HopacTest/WorkersPool2.fsx
+++ HopacTest/WorkersPool2.fsx
@@ -1,14 +1,14 @@
-﻿#r "packages/Hopac.0.0.0.31/lib/net45/Hopac.Core.dll"
-#r "packages/Hopac.0.0.0.31/lib/net45/Hopac.dll"
-#r "packages/ExtCore.0.8.43/lib/net40/ExtCore.dll"
+﻿#r "packages/Hopac.0.0.0.32/lib/net45/Hopac.Core.dll"
+#r "packages/Hopac.0.0.0.32/lib/net45/Hopac.dll" 
+#r "packages/ExtCore.0.8.43/lib/net40/ExtCore.dll" 
 
-open Hopac
+open Hopac 
 open Hopac.Job.Infixes
 open Hopac.Alt.Infixes
 
-type Pool<'msg, 'res>(degreeOfParallelism: int, source: Alt<'msg>, worker: 'msg -> Choice<'res, 'msg * exn>) =
+type Pool<'msg, 'res>(degreeOfParallelism: int, source: Alt<'msg>, worker: 'msg -> Job<Choice<'res, 'msg * exn>>) =
     let degreeOfParallelism = MVar.Now.createFull degreeOfParallelism
-    let dopChanged = ch<unit>()
+    let dopChanged = ch<unit>() 
     let workDone = ch<Choice<'res, 'msg * exn>>()
     let failedMessages = mb()
     let getMessage workerCount = 
@@ -21,7 +21,7 @@ type Pool<'msg, 'res>(degreeOfParallelism: int, source: Alt<'msg>, worker: 'msg 
     
     let pool = Job.iterateServer 0 <| fun workerCount ->
         Alt.select [ getMessage workerCount >>=? fun msg -> 
-                         job { let res = worker msg
+                         job { let! res = worker msg
                                return! Ch.give workDone res } |> Job.start >>% workerCount + 1
                      workDone >>=? fun res ->
                         match res with
@@ -34,14 +34,25 @@ type Pool<'msg, 'res>(degreeOfParallelism: int, source: Alt<'msg>, worker: 'msg 
         MVar.take degreeOfParallelism >>. MVar.fill degreeOfParallelism value >>. Ch.give dopChanged () |> start
 
 module Test =
+    open System
+
     let mb = mb<int>()
-    let pool = Pool<int, unit>(3, mb, (fun msg -> printfn "[worker] Received %A" msg; Choice1Of2()))
-    start (Mailbox.send mb 1)
+    
+    let pool = Pool<int, unit>(3, mb, (fun msg -> job {
+                   //printfn "[worker] Received %A. Sleeping..." msg
+                   //do! Timer.Global.sleep (TimeSpan.FromMinutes 10.)
+                   return Choice1Of2()
+                   //printfn "[worker] Received %A. Returning error..." msg
+                   //return Choice2Of2(msg, Exception()) 
+                   }))
 
+    for i in 1..1000000 do run (Mailbox.send mb i)
+    
 
-    //pool.SetCapacity 4
+    pool.SetDegreeOfParallelism 10
     //pool.Add 20
 
 //    for i in 1..10000000 do pool.Add i
+
 
 
