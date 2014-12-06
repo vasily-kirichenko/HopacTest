12cdc4111624488b5e0bf9c4cc74b85cbe75027e
vasily-kirichenko <vaskir@gmail.com>
2014-12-06 17:11:42 +0300

    *

--- HopacTest/Alts.fsx
+++ HopacTest/Alts.fsx
@@ -1,27 +1,40 @@
-﻿#r "packages/Hopac.0.0.0.31/lib/net45/Hopac.Core.dll"
-#r "packages/Hopac.0.0.0.31/lib/net45/Hopac.dll"
+﻿#r "packages/Hopac.0.0.0.32/lib/net45/Hopac.Core.dll"
+#r "packages/Hopac.0.0.0.32/lib/net45/Hopac.dll"
 
-open System
 open Hopac
 open Hopac.Job.Infixes
 open Hopac.Alt.Infixes
 
-let addCh = ch<int>()
-let subCh = ch<int>()
-let readCh = ch<int>()
+let reqCh = ch()
+let respCh = ch()
 
-let accum: Alt<unit> =
-    let rec loop sum =
-        Alt.choose [ Ch.Alt.take addCh >>=? fun x -> upcast loop (sum + x)
-                     Ch.Alt.take subCh >>=? fun x -> upcast loop (sum - x)
-                     Ch.Alt.give readCh sum >>=? fun _ -> upcast loop sum ]
-    loop 0
-
-let server =
+let server() = 
     Job.iterateServer () <| fun _ ->
-        Alt.select [ accum >>%? printfn "accum!"
-                     Timer.Global.timeOut (TimeSpan.FromSeconds 2.) >>%? printfn "timeout!" ]
+        Ch.take reqCh >>= fun x -> Ch.give respCh (x * x)
 
-start server
-run (Ch.give addCh 1)
-run (Ch.take readCh |>> printfn "%A") 
+let call x = Ch.Alt.give reqCh x >>.? Ch.take respCh 
+
+start (server())
+run (call 1000)
+
+//let addCh = ch<int>()
+//let subCh = ch<int>()
+//let readCh = ch<int>()
+//
+//let accum: Alt<unit> =
+//    let rec loop sum =
+//        Alt.choose [ Ch.Alt.take addCh >>=? fun x -> upcast loop (sum + x)
+//                     Ch.Alt.take subCh >>=? fun x -> upcast loop (sum - x)
+//                     Ch.Alt.give readCh sum >>=? fun _ -> upcast loop sum ]
+//    loop 0
+//
+//let server =
+//    Job.iterateServer () <| fun _ ->
+//        Alt.select [ accum >>%? printfn "accum!"
+//                     Timer.Global.timeOut (TimeSpan.FromSeconds 2.) >>%? printfn "timeout!" ]
+//
+//start server
+//run (Ch.give addCh 1)
+//run (Ch.take readCh |>> printfn "%A") 
+//run (Alt.pick accum)
+
