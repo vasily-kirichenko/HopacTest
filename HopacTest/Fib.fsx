86677d8d9b6d11ac8631919617a36af04e590954
Vasily Kirichenko <Vasily.Kirichenko@kaspersky.com>
2014-12-09 16:05:31 +0300

    *

--- HopacTest/Fib.fsx
+++ HopacTest/Fib.fsx
@@ -9,7 +9,7 @@ let rec fib n = Job.delay <| fun () ->
   if n < 2L then
     Job.result n
   else
-    fib (n-2L) <&> fib (n-1L) |>> fun (x, y) ->
+    fib (n-2L) <*> fib (n-1L) |>> fun (x, y) ->
     x + y
 
 (*
@@ -30,5 +30,7 @@ Real: 00:00:08.638, CPU: 00:00:08.860, GC gen0: 229, gen1: 0, gen2: 0
 val it : int64 = 39088169L
 
 4. Parallel, server GC
-
+> run (fib 38L);;
+Real: 00:00:02.897, CPU: 00:00:17.425, GC gen0: 197, gen1: 0, gen2: 0
+val it : int64 = 39088169L
 *)
