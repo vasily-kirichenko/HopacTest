be3d131c2b3818ab05ee059dee687b457869c380
vasily-kirichenko <vaskir@gmail.com>
2015-01-03 10:55:25 +0300

    *

---
+++ HopacTest/JoinadsOnePlaceBuffer.fsx
@@ -0,0 +1,73 @@
+﻿#load "Header.fsx"
+
+open Hopac
+open Hopac.Infixes
+open Hopac.Alt.Infixes
+open Hopac.Job.Infixes
+
+let put = ch<string>()
+let get = ch<string>()
+let empty = mb<unit>()
+let contains = mb<string>()
+
+run (empty <<-+ ())
+
+Alt.choose [ put >>= fun s -> empty >>% s >>= fun s -> contains <<-+ s
+             get >>= empty <<-+ () ]   
+
+  match! put, empty, get, contains with 
+  | (s, repl), (), ?, ? -> return react {
+    yield contains.Put(s)
+    yield repl.Reply() }
+  | ?, ?, repl, v -> return react {
+    yield repl.Reply(v)
+    yield empty.Put(()) } }
+
+// Repeatedly try to put value into the buffer
+async { do! Async.Sleep(1000)
+        for i in 0 .. 10 do
+          printfn "putting: %d" i
+          do! put.AsyncCall(string i)
+          do! Async.Sleep(500) }
+|> Async.Start
+
+// Repeatedly read values from the buffer and print them
+async { while true do 
+          do! Async.Sleep(250)
+          let! v = get.AsyncCall()
+          printfn "got: %s" v }
+|> Async.Start
+
+//let onePlaceBuffer() = 
+//  let put = SyncChannel<string, unit>()
+//  let get = SyncChannel<string>()
+//  let empty = Channel<unit>()
+//  let contains = Channel<string>()
+//  
+//  // Initially, the buffer is empty
+//  empty.Call(())
+//
+//  join {
+//    match! put, empty, get, contains with 
+//    | (s, repl), (), ?, ? -> return react {
+//      yield contains.Put(s)
+//      yield repl.Reply() }
+//    | ?, ?, repl, v -> return react {
+//      yield repl.Reply(v)
+//      yield empty.Put(()) } }
+//
+//  // Repeatedly try to put value into the buffer
+//  async { do! Async.Sleep(1000)
+//          for i in 0 .. 10 do
+//            printfn "putting: %d" i
+//            do! put.AsyncCall(string i)
+//            do! Async.Sleep(500) }
+//  |> Async.Start
+//
+//  // Repeatedly read values from the buffer and print them
+//  async { while true do 
+//            do! Async.Sleep(250)
+//            let! v = get.AsyncCall()
+//            printfn "got: %s" v }
+//  |> Async.Start
+  
