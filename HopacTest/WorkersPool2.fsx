4d349b7b16ad74b339456b6817bdbff79e7040a9
vasily-kirichenko <vaskir@gmail.com>
2015-01-05 11:44:11 +0300

    rewrite WorkersPool2 align Vesa Karvonen comments

--- HopacTest/WorkersPool2.fsx
+++ HopacTest/WorkersPool2.fsx
@@ -5,39 +5,29 @@ open Hopac.Job.Infixes
 open Hopac.Alt.Infixes
 open Hopac.Infixes
 
-type Pool<'msg, 'res, 'error>(degreeOfParallelism: int, source: Alt<'msg>, worker: 'msg -> Job<Choice<'res, 'msg * 'error>>) =
-    let degreeOfParallelism = MVar.Now.createFull degreeOfParallelism
-    let dopChanged = ch<unit>() 
+type Pool<'msg, 'res, 'error>(degree: int, source: Alt<'msg>, worker: 'msg -> Job<Choice<'res, 'msg * 'error>>) =
+    let setDegree = ch<int>() 
     let workDone = ch<Choice<'res, 'msg * 'error>>()
     let failedMessages = mb()
     
-    let getMessage workerCount = 
-        let get() = Alt.guard (job {
-            let! dop = MVar.read degreeOfParallelism
-            printfn "Dop read: %d. Worker count: %d" dop workerCount
-            return 
-                if workerCount < dop then
-                    printfn "Getting from source..."
-                    source <|>? failedMessages 
-                else
-                    printfn "Alt.never"
-                    Alt.never() })
-             
-        get() <|>? (dopChanged >>=? fun _ -> printfn "Dop changed!"; get())
-    
-    let pool = Job.iterateServer 0 <| fun workerCount ->
-        Alt.choose [ getMessage workerCount >>=? fun msg ->
-                        Job.start (worker msg >>= Ch.send workDone) >>% workerCount + 1
-                     workDone >>=? fun r ->
-                        printfn "Work done: %A (worker count = %d)" r workerCount
-                        match r with
-                        | Choice1Of2 _ -> Job.result (workerCount - 1)
-                        | Choice2Of2 (msg, _) -> failedMessages <<-+ msg >>% workerCount - 1 
-                   ]
+    let pool = Job.iterateServer (degree, 0)  <| fun (degree, usage) ->
+        (setDegree |>>? fun dop -> (dop, usage)) <|>? 
+        (workDone |>>? fun _ -> degree, usage - 1) <|>?
+        (if usage < degree then
+            (source <|>? failedMessages) >>=? fun x -> 
+            Job.delayWith worker x >>= (fun r ->
+                match r with
+                | Choice1Of2 _ -> Job.result r
+                | Choice2Of2 (msg, _) -> failedMessages <<-+ msg >>% r) >>= fun r ->
+                workDone <-- r
+            |> Job.queue
+            >>% (degree, usage + 1)
+         else 
+            Alt.never()) 
+            
     do start pool
     
-    member __.SetDegreeOfParallelism value =
-        degreeOfParallelism >>. MVar.fill degreeOfParallelism value >>. (dopChanged <-+ ()) |> run
+    member __.SetDegree value = setDegree <-+ value |> run
 
 module Test =
     open System
@@ -45,15 +35,15 @@ module Test =
 
     let mb = mb<int>()
     
-    let pool = Pool<int, unit, exn>(3, mb, (fun msg -> job {
+    let pool = Pool<int, int, exn>(2, mb, (fun msg -> job {
                    printfn "[worker] Received %A. Sleeping..." msg
                    do! Timer.Global.timeOut (TimeSpan.FromSeconds 1.)
-                   return Choice1Of2() }))
+                   return Choice1Of2 msg }))
 
     [1..1000] |> Seq.Con.iterJob (Mailbox.send mb) |> run
     
 
-    pool.SetDegreeOfParallelism 4
+    pool.SetDegree 1
     //pool.Add 20
 
 //    for i in 1..10000000 do pool.Add i
